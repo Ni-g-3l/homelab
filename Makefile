49ab84c936673d104dc2819898f32aa1a2380633
Nig3l <nig3lpro@gmail.com>
2025-07-17 18:45:44 +0200

    feat(scripts): Create Makefile rule to start all services at same time

--- Makefile
+++ Makefile
@@ -6,3 +6,9 @@ install:
 
 new:
 	. .venv/bin/activate && copier copy services/template $(CURRENT_DIR)services/
+
+run:
+	. .venv/bin/activate && python scripts/runtime/run_commands.py ${CURRENT_DIR}services/ --cmd 'make'
+
+update:
+	. .venv/bin/activate && python scripts/runtime/run_commands.py ${CURRENT_DIR}services/ --cmd 'make update'
---
+++ scripts/runtime/run_commands.py
@@ -0,0 +1,28 @@
+import os
+import subprocess
+
+def run_command_in_services(root, cmd):
+    
+    for dir in os.listdir(root):
+        if not os.path.isdir(dir):
+            continue
+        service_directory = os.path.join(root, dir)
+        print(f"Run command {cmd} in service '{dir}'")
+        try:
+            subprocess.run(cmd.split(" "), cwd=service_directory)
+            print("Service started")
+        except subprocess.CalledProcessError as e: 
+            print(f"Error on command running, Command : '{cmd}', Service Folder : '{service_directory}'")
+
+
+
+if __name__ == "__main__":
+    import argparse
+
+    parser = argparse.ArgumentParser(description="Run `make` in all service folders.")
+    parser.add_argument('root', nargs='?', default='.', help="Root directory to search (default: current directory).")
+    parser.add_argument('--cmd', default="make", help="Command to run in all services folder")
+    args = parser.parse_args()
+
+    run_command_in_services(args.root, args.cmd)
+
