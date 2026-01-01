902ad6bbe9d8f0ae69e932f9c5c106b5734aedf1
Nig3l <cots.max@gmail.com>
2026-01-01 19:22:47 +0100

    Update taskfile to allow run command in multiple folders

--- scripts/runtime/run_commands.py
+++ scripts/runtime/run_commands.py
@@ -1,9 +1,10 @@
 import os
 import subprocess
 
+
 def run_command_in_services(root, cmd):
-    
     for dir in os.listdir(root):
+        dir = os.path.join(root, dir)
         if not os.path.isdir(dir):
             continue
         service_directory = os.path.join(root, dir)
@@ -11,18 +12,25 @@ def run_command_in_services(root, cmd):
         try:
             subprocess.run(cmd.split(" "), cwd=service_directory)
             print("Service started")
-        except subprocess.CalledProcessError as e: 
-            print(f"Error on command running, Command : '{cmd}', Service Folder : '{service_directory}'")
-
+        except subprocess.CalledProcessError as e:
+            print(
+                f"Error on command running, Command : '{cmd}', Service Folder : '{service_directory}'"
+            )
 
 
 if __name__ == "__main__":
     import argparse
 
     parser = argparse.ArgumentParser(description="Run `make` in all service folders.")
-    parser.add_argument('root', nargs='?', default='.', help="Root directory to search (default: current directory).")
-    parser.add_argument('--cmd', default="make", help="Command to run in all services folder")
+    parser.add_argument(
+        "root",
+        nargs="?",
+        default=".",
+        help="Root directory to search (default: current directory).",
+    )
+    parser.add_argument(
+        "--cmd", default="make", help="Command to run in all services folder"
+    )
     args = parser.parse_args()
 
     run_command_in_services(args.root, args.cmd)
-
